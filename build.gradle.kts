1706263e37ac0b5d13bb7dae4f26c6045f25083a
msnzdev <msnzdev@proton.me>
2026-07-05 19:04:57 +0200

    Delete all

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-// Configuración raíz del proyecto
-plugins {
-    id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
-}
