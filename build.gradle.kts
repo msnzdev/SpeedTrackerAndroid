f8ed011347845330cfae5c30d6b77ce936dba0b8
msnzdev <msnzdev@proton.me>
2026-07-12 13:15:25 +0200

    Upgrade Kotlin plugin versions to 2.0.21
    Updated Kotlin plugin versions for Android.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Configuración raíz del proyecto
 plugins {
     id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
